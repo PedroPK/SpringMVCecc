0629b200706fcb5ee3b7fd67f06226d16f554bad
pedropk <pedropk@gmail.com>
2019-03-25 13:27:04 -0300

    Setting the Format SQL Property

--- Spring/src/br/spring/mvc/configs/ConfigurationJPA.java
+++ Spring/src/br/spring/mvc/configs/ConfigurationJPA.java
@@ -83,6 +83,7 @@ public class ConfigurationJPA {
 		properties.setProperty("hibernate.hbm2ddl.auto",							"create");
 		properties.setProperty("hibernate.dialect",									"org.hibernate.dialect.H2Dialect");
 		properties.setProperty("hibernate.show_sql",								"true");
+		properties.setProperty("hibernate.format_sql",								"true");
 		
 		return properties;
 	}
