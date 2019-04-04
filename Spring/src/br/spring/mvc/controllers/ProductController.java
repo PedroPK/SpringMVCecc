85c168bb2dfc88e9c62ec04624fd71c16b98cad6
pedropk <pedropk@gmail.com>
2019-04-04 12:57:12 -0300

    Some Objects are already been sent to a JSP.
    Still have to make a List do work.
    
    https://github.com/livrospringmvc/lojacasadocodigo/blob/2df534243474f9226ff5086a0080db1def3620eb/src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java

--- Spring/src/br/spring/mvc/controllers/ProductController.java
+++ Spring/src/br/spring/mvc/controllers/ProductController.java
@@ -1,9 +1,15 @@
 		package br.spring.mvc.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.spring.mvc.model.daos.ProductDAO;
 import br.spring.mvc.model.entities.Product;
@@ -12,44 +18,75 @@ import br.spring.mvc.model.entities.Product;
 @Transactional
 public class ProductController {
 	
+	private Logger logger;
+	
 	@Autowired
 	private ProductDAO aDAO;
 	
+	public ProductController() {
+		// Logging messages
+		this.logger = LoggerFactory.getLogger("spring.mvc.controllers.ProductController");
+		this.logger.info("ProductController instancialized");
+	}
+	
 	@RequestMapping("/registerProduct")
-	public String submit(Product pProduto) {
-		System.out.println("ProductController - /registerProduct");
-		if ( pProduto != null ) {
-			System.out.println(pProduto.toString());
+	public ModelAndView submit(Product pProduto) {
+		// Logging messages
+		logger.debug("ProductController - /registerProduct");
+		
+		// This constructor defines to where the Request should be redirected
+		ModelAndView mv = new ModelAndView("cadastroProdutos");
+		if (
+				pProduto	!= null		&&
+				pProduto.hasAnyAttributeValid()
+		) {
+			// Logging messages
+			logger.debug(pProduto.toString());
 			
+			// Insert the Product
 			this.aDAO.save(pProduto);
+			
+			// Select the inserted Product
+			Product insertedProduct = this.aDAO.select(pProduto);
+			
+			// Mount a List of Products, that may be listed at JSP any moment in the future
+			List<Product> listInsertedProducts = new ArrayList<Product>();
+			listInsertedProducts.add(insertedProduct);
+			
+			// This method adds an object that has to be available in JSP
+			mv.addObject("product",					insertedProduct);
+			mv.addObject("listInsertedProducts",	listInsertedProducts);
+			
+			mv = new ModelAndView("productList");
 		} else {
-			System.out.println("Produto Nulo!");
+			// Logging messages
+			logger.debug("Produto Nulo!");
 		}
 		
-		return "products/form";
+		return mv;
 	}
 	
 	@RequestMapping("/Spring/products")
 	public String submit2() {
-		System.out.println("ProductController - /Spring/products");
+		logger.debug("ProductController - /Spring/products");
 		return "cadastroProdutos";
 	}
 	
 	@RequestMapping("/products")
 	public String showForms() {
-		System.out.println("ProductController - /Spring/products");
+		logger.debug("ProductController - /Spring/products");
 		return "cadastroProdutos";
 	}
 	
 	@RequestMapping("/products/form")
 	public String form() {
-		System.out.println("ProductController - /products/form");
+		logger.debug("ProductController - /products/form");
 		return "products/form";
 	}
 	
 	@RequestMapping("/")
 	public String index() {
-		System.out.println("Página Inicial");
+		logger.debug("Página Inicial");
 		return "index.jsp";
 	}
 	
